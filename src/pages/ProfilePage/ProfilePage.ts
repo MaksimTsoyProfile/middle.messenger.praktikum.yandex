e6e25d89890cee14d4a24dced6b803b66d1eb2a9
Maksim Tsoy <maksimtsoy1990@mail.ru>
2024-03-06 13:03:21 +0500

    add: profile page

--- src/pages/ProfilePage/ProfilePage.ts
+++ src/pages/ProfilePage/ProfilePage.ts
@@ -8,5 +8,20 @@ export default `
     <div class='profile-container__input'>
       {{> InputField name='email' type='email' value='pochta@yandex.ru' label='Почта' disabled=true }}
     </div>
+    <div class='profile-container__input'>
+        {{> InputField name='login' type='text' value='pochta@yandex.ru' label='Логин' disabled=true }}
+    </div>
+    <div class='profile-container__input'>
+        {{> InputField name='first_name' type='text' value='pochta@yandex.ru' label='Имя' disabled=true }}
+    </div>
+    <div class='profile-container__input'>
+        {{> InputField name='second_name' type='text' value='pochta@yandex.ru' label='Фамилия' disabled=true }}
+    </div>
+    <div class='profile-container__input'>
+        {{> InputField name='display_name' type='text' value='pochta@yandex.ru' label='Имя в чате' disabled=true }}
+    </div>
+    <div class='profile-container__input'>
+        {{> InputField name='phone' type='text' value='pochta@yandex.ru' label='Телефон' disabled=true }}
+    </div>
   </div>
 `;
