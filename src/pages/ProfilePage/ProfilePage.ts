2b818913197421cfe17927cf3fa674ab396395aa
Maksim Tsoy <maksimtsoy1990@mail.ru>
2024-03-06 13:23:34 +0500

    fix: profile page style

--- src/pages/ProfilePage/ProfilePage.ts
+++ src/pages/ProfilePage/ProfilePage.ts
@@ -6,22 +6,33 @@ export default `
       {{> Avatar src='' }}
     </div>
     <div class='profile-container__input'>
-      {{> InputField name='email' type='email' value='pochta@yandex.ru' label='Почта' disabled=true }}
+      {{> InputField name='email' type='email' value='pochta@yandex.ru' label='Почта' disabled=false }}
     </div>
     <div class='profile-container__input'>
-        {{> InputField name='login' type='text' value='pochta@yandex.ru' label='Логин' disabled=true }}
+        {{> InputField name='login' type='text' value='ivanov' label='Логин' disabled=true }}
     </div>
     <div class='profile-container__input'>
-        {{> InputField name='first_name' type='text' value='pochta@yandex.ru' label='Имя' disabled=true }}
+        {{> InputField name='first_name' type='text' value='Иван' label='Имя' disabled=true }}
     </div>
     <div class='profile-container__input'>
-        {{> InputField name='second_name' type='text' value='pochta@yandex.ru' label='Фамилия' disabled=true }}
+        {{> InputField name='second_name' type='text' value='Иванов' label='Фамилия' disabled=true }}
     </div>
     <div class='profile-container__input'>
-        {{> InputField name='display_name' type='text' value='pochta@yandex.ru' label='Имя в чате' disabled=true }}
+        {{> InputField name='display_name' type='text' value='Иван' label='Имя в чате' disabled=true }}
     </div>
     <div class='profile-container__input'>
-        {{> InputField name='phone' type='text' value='pochta@yandex.ru' label='Телефон' disabled=true }}
+        {{> InputField name='phone' type='text' value='+7 (909) 967 30 30' label='Телефон' disabled=true }}
+    </div>
+    <div class='profile-container__links'>
+      <div class='profile-container__links__item'>
+        {{> Link page='' text='Изменить данные' }}
+      </div>
+      <div class='profile-container__links__item'>
+        {{> Link page='' text='Изменить пароль' }}
+      </div>
+      <div class='profile-container__links__item-danger'>
+        {{> Link page='' text='Выйти' danger=true }}
+      </div>
     </div>
   </div>
 `;
