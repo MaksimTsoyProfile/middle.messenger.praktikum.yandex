20723db6398fdaeef5a94084aaf34ce49f4e975d
Maksim Tsoy <maksimtsoy1990@mail.ru>
2024-03-08 15:32:16 +0500

    fix: server js

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import path from 'path';
 
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist'));
+});
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
@@ -12,4 +17,5 @@ app.listen(PORT, function () {
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.status(200);
 });
